1826fd90c7b6d43cfc6d8d551a5d4b75d524f6f8
Firmino Andre <andrezfirmino@gmail.com>
2021-10-04 02:48:43 -0300

    ajustes na pagina principal do projeto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,30 @@
 @section('title', 'Home')
 
 @section('content')
-<h1>Página Inicial</h1>
+<div id="search-conteiner" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar um evento">
+    </form>
+</div>
+
+<div id="events-container" class="col-md-12">
+    <h2>Eventos encontrados</h2>
+    <p>Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row row-cols-1 row-cols-md-3 g-4">
+        @foreach($event as $events)
+            <div class="card col-md-3">
+                <img src="/img/event1.jpg" alt="{{ $events->title }}">
+                <div class="card-body">
+                    <p class="card-date">10/09/2021</p>
+                    <h4 class="card-title">{{ $events->title }}</h4>
+                    <p class="card-participantes">X participantes</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
 
 
